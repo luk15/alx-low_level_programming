7c07bc949e82ac24f9dd83866607b9ccf2e0829a
Albert Anyika <1034777@cuea.edu>
2022-03-10 12:40:17 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *  main - main function
+ *
+ *  return - 0 on success
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
