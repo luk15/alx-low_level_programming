97009bed29749810e309d4694302c323882fd60f
Albert Anyika <1034777@cuea.edu>
2022-03-11 01:41:14 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @c - var of type char
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
