4bc46aa840be94bf50b449ee0c765e86dfc3d939
Albert Anyika <1034777@cuea.edu>
2022-03-11 04:26:23 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *@i - int type
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
