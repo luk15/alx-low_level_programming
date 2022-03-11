69ae6cd43f36fb69a5dabde8dc10f2c3ceb19017
Albert Anyika <1034777@cuea.edu>
2022-03-11 04:12:04 -0800

    loops

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,24 +2,21 @@
 /**
  * main - main function
  * @c - var of type char
- * @C - var of type char
  *
  * Return: 0 on success
+ *
  */
 int main(void)
 {
 	char c = 'a';
-	char C = 'A';
 
 	while (c <= 'z')
 	{
-		putchar(c);
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
 		c++;
-	}
-	while (C <= 'Z')
-	{
-		putchar(C);
-		C++;
 	}
 	putchar('\n');
 	return (0);
