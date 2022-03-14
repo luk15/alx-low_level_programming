ebd74e13ffd67eab0ea51e4d38bc8df81e7bec1f
Albert Anyika <1034777@cuea.edu>
2022-03-13 23:57:05 -0700

    unique single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: 0 on succes
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
