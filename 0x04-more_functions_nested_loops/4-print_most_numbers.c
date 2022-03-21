9525761df6ec145a6c7c676ba22fb02923121f20
Albert Anyika <1034777@cuea.edu>
2022-03-21 01:56:55 -0700

    more_numberss

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 exept 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
