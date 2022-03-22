f7b4c60a68222309c81cc14bd27d9f6307dce593
Albert Anyika <1034777@cuea.edu>
2022-03-22 00:50:11 -0700

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of times the line is printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
