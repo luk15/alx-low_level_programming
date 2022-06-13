2a1f48ff5b7d17d3c52c1160ce3889a7b4e74d87
Albert Disi <disialbert1998@gmail.com>
2022-06-13 23:30:30 +0300

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,18 @@ void times_table(void)
 {
 	int i, j, n;
 
-	for (i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
+
 			if ((n / 10) == 0)
 			{
 				if (j != 0)
 					_putchar(' ');
 				_putchar(n + '0');
+
 				if (j == 9)
 					continue;
 				_putchar(',');
@@ -37,4 +39,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-
