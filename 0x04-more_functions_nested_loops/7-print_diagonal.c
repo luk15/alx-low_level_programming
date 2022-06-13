fc80e48ce0cd22a014eed6fe3068c6185a795247
Albert Disi <disialbert1998@gmail.com>
2022-06-13 23:44:36 +0300

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - reads from the main header
+ * @n: parameter of type int
+ * _putchar: prints the output
+ * Return: Always 0 success
+ */
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
